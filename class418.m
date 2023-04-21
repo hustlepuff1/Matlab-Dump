85961c5cf2df0c5fe354a99df7bcaac3ee2ec73f
hustlepuff1 <henryjin1208@gmail.com>
2023-04-21 13:32:47 +0900

    added AirLab3

--- class418.m
+++ class418.m
@@ -1,4 +1,4 @@
-clc; close all;
+clc; clear all; close all;
 numG = 8;
 denG = [1 10 16];
 G = tf(numG, denG);
@@ -7,10 +7,12 @@ t = 0:0.1:5;
 Kp = 1:2:9;
 for i = 1:length(Kp)
     numD = Kp(i);
-    denD = [1, 0];
+    denD = 1;
     D = tf(numD, denD);
     cltf = feedback(D*G, 1);
     y(:, i) = step(cltf, t);
 end
 
 plot(t, y, 'LineWidth', 2);
+
+
